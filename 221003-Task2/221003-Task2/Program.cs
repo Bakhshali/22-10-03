ebd7e807fd108b1029e525aae976ffa5fd36e9a8
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-10 12:54:43 +0400

    Task 2 added

---
+++ 221003-Task2/221003-Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _221003_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 23, 69, 45, 67, 39, 0 };
+            Biggest (array);
+            
+        }
+        public static void Biggest (int[]array)
+        {   
+            int big=array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > big)
+                {
+                    big = array[i];
+                    
+                }
+                {
+
+                }
+                
+            }
+            Console.WriteLine("Biggest number is - " + big);
+        }           
+        }
+    }
