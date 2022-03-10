e4461eb43f6af8a2f70fd762c8a4e67f2ed7291e
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-10 12:54:19 +0400

    Task 1 added

---
+++ 221003-Task1/221003-Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _221003_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Written = Convert.ToInt32(Console.ReadLine());
+            int Reading = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Value(Written, Reading)); 
+
+
+        }
+        public static int Value (int Written, int Reading)
+        {
+            int count = 0;
+            int swap = Written;
+            while (swap > 1)
+            {
+                swap /= 10;
+                count++;
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        
+                    }
+                }                
+            }
+            return Written;
+        }
+    }
+}
